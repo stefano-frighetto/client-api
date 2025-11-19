287617574a927f29da86699189abf38d226fe7c1
Stéfano Frighetto <106117483+stefano-frighetto@users.noreply.github.com>
2025-11-19 00:05:52 -0300

    Replace MSSQLServer with PostgreSQL (#2)
    * Replace SQL Server with PostgreSQL
    
    * Update Client Model & Entity Framework Mapping
    
    * Date compatibility and arrange

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,8 +14,19 @@ namespace ClientApi.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Client>().ToTable("Client");
-        }
+            modelBuilder.Entity<Client>().ToTable("clientes");
 
+            modelBuilder.Entity<Client>(entity =>
+            {
+                entity.Property(e => e.ClientId).HasColumnName("id");
+                entity.Property(e => e.FirstName).HasColumnName("nombre");
+                entity.Property(e => e.LastName).HasColumnName("apellido");
+                entity.Property(e => e.CorporateName).HasColumnName("razon_social");
+                entity.Property(e => e.CUIT).HasColumnName("cuit");
+                entity.Property(e => e.Birthdate).HasColumnName("fecha_nacimiento").HasColumnType("date");
+                entity.Property(e => e.CellPhone).HasColumnName("telefono_celular");
+                entity.Property(e => e.Email).HasColumnName("email");
+            });
+        }
     }
 }
--- Models/Client.cs
+++ Models/Client.cs
@@ -5,18 +5,28 @@ namespace ClientApi.Models
     public class Client
     {
         public int ClientId { get; set; }
+
         [Required]
         public string FirstName { get; set; }
+
         [Required]
         public string LastName { get; set; }
-        public DateTime? Birthdate { get; set; }
+
+        [Required]
+        public string CorporateName { get; set; }
+
         [Required]
         [RegularExpression(@"^\d{2}-\d{8}-\d$", ErrorMessage = "Invalid CUIT. Must be XX-XXXXXXXX-X")]
         public string CUIT { get; set; }
-        public string? Address { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+
         [Required]
         [RegularExpression(@"^\d{10}$", ErrorMessage = "Invalid phone number. Must be 10 consecutive numbers only.")]
         public string CellPhone { get; set; }
+
         [Required]
         [EmailAddress]
         public string Email { get; set; }
